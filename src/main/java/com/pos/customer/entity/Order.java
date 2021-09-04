d9c56c0b19b6587e72ec655482f4f23d63461359
S-Silvan <silvan8124@gmail.com>
2021-09-04 23:10:33 +0530

    Order and Order Item Entity addition

---
+++ src/main/java/com/pos/customer/entity/Order.java
@@ -0,0 +1,42 @@
+package com.pos.customer.entity;
+
+import java.time.LocalDate;
+import java.util.List;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name="orders")
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+public class Order {
+	@Id @GeneratedValue @Column(name="order_id")
+	private Integer id;
+	@Column(name="order_date")
+	private LocalDate date;
+	@Column(name="order_total_price")
+	private Double totalPrice;
+	@Column(name="order_discount")
+	private Float discount;
+	@Column(name="order_payment_mode")
+	private String modeOfPayment;
+	@Column(name="order_status")
+	private String status;
+	@Column(name="order_tracking")
+	private String tracking;
+	
+	@ManyToOne
+	private Address address;
+	@ManyToOne
+	private Customer customer;
+}
---
+++ src/main/java/com/pos/customer/entity/OrderItem.java
@@ -0,0 +1,30 @@
+package com.pos.customer.entity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name="orders")
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+public class OrderItem {
+	@Id @GeneratedValue @Column(name="order_item_id")
+	private Integer id;
+	@Column(name="order_item_quantity")
+	private Integer quantity;
+	@Column(name="order_item_price")
+	private Double price;
+	@ManyToOne
+	private Order order;
+	@ManyToOne
+	private Product product;
+}
